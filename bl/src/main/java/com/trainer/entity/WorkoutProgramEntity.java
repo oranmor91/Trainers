8b548001f2802cddf4e771ff61d4a51c04e9bac7
Neri Richter <neri.richter@ca.com>
2018-05-20 23:14:39 +0300

    create workoutProgram entity

--- bl/src/main/java/com/trainer/entity/WorkoutProgramEntity.java
+++ bl/src/main/java/com/trainer/entity/WorkoutProgramEntity.java
@@ -1,9 +1,21 @@
 package com.trainer.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 import com.trainer.visitors.BaseVisitor;
 
+@Entity
+@Table(name="work_prog")
 public class WorkoutProgramEntity extends BaseEntity{
 
 	private static final long serialVersionUID = 1L;
@@ -17,9 +29,21 @@ public class WorkoutProgramEntity extends BaseEntity{
 	@Column
 	private String notes;
 	
+	@ManyToMany(cascade = { CascadeType.ALL}, fetch=FetchType.EAGER)
+	@JoinTable(name = "workout_program_link", joinColumns = { @JoinColumn(name = "work_prog_id") }, inverseJoinColumns = { @JoinColumn(name = "workouts_id") })
+	private List<WorkoutEntity> workouts = new ArrayList<WorkoutEntity>();
+	
 	@Override
 	public Object accept(BaseVisitor visitor, Object... obj) {
 		return visitor.visit(this, obj);
+	}
+
+	public List<WorkoutEntity> getWorkouts() {
+		return workouts;
+	}
+
+	public void setWorkouts(List<WorkoutEntity> workouts) {
+		this.workouts = workouts;
 	}
 
 }
