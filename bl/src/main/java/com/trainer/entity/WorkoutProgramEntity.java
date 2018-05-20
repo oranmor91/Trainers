06cabf02217c14367e2fc93e78b56a9613ea3419
Neri Richter <neri.richter@ca.com>
2018-05-20 23:35:46 +0300

    added getters setters

--- bl/src/main/java/com/trainer/entity/WorkoutProgramEntity.java
+++ bl/src/main/java/com/trainer/entity/WorkoutProgramEntity.java
@@ -46,4 +46,28 @@ public class WorkoutProgramEntity extends BaseEntity{
 		this.workouts = workouts;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
 }
